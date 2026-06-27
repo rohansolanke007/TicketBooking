f7c88cc8475dde145e9e8b360ce7b1b597be76ff
rohansolanke007 <rohansolanke910@gmail.com>
2026-06-27 16:00:00 +0530

    Implement train class with booking and status methods
    Added a train class with methods for booking and status.

--- code.py
+++ code.py
@@ -1,1 +1,24 @@
+import random
 
+class train:
+    
+
+
+    def __init__(self,trainNo):
+        self.trainNo = trainNo
+
+
+    def book(self,fro,to):
+        print(f"Train is booked in train no: {self.trainNo} from {fro} to {to}")
+
+    def getstatus(self,fro ,to):
+        print(f"Train no: {self.trainNo} is running on time")
+
+
+t = train(312121)
+
+t.book("pune" , "delhi")
+
+t.getstatus("pune" , "delhi")
+
+t.getFare("pune" , "delhi")
